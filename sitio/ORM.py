eb40d1c65752ddcb461fc455584eb00af90dbe23
Kevin <servidoryei@gmail.com>
2018-03-28 01:53:41 -0600

    se revirtio la eliminacion de ENV
    lo que borre alv lo traje  de regreso

--- sitio/ORM.py
+++
@@ -1,9 +0,0 @@
-class sitio:
-	def __init__(self,_id,_nombre,_direccion):
-		self.id=_id
-		self.nombre=_nombre
-		self.direccion = _direccion
-
-	def __json__(self):
-		json = {"id" : self.id,"nombre" : self.nombre, "direccion" : self.direccion}
-		return json
--- sitio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class sitioConfig(AppConfig):
-    name = 'sitio'
